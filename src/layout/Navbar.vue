ead05f1318d26c70711e0abdcbd358efe48b14d3
VIDUSHISAINI29 <vidushisaini29@gmail.com>
2024-11-15 21:33:07 +0530

    navbar changed

--- src/layout/Navbar.vue
+++ src/layout/Navbar.vue
@@ -4,15 +4,15 @@
 
 <template>
     <div class="tw-w-full">
-        <div class="  tw-items-center  tw-w-full tw-text-[#1c1b22] tw-text-lg tw-font-semibold tw-flex tw-p-2 tw-px-8 tw-justify-between tw-bg-[#4B5563]">
+        <div class="  tw-items-center tw-shadow-2xl tw-w-full tw-text-[#1c1b22] tw-text-lg tw-font-semibold tw-flex tw-p-3 tw-px-8 tw-justify-between tw-bg-[#fff]">
         <div  class="tw-cursor-pointer">
       <img class="tw-w-10 tw-h-10    " src="/Images/football.webp" alt="">   
       </div>
         <div class=" tw-flex tw-gap-10  ">
-            <span class="tw-cursor-pointer tw-flex tw-text-[#000] tw-items-center tw-justify-center tw-rounded-md tw-p-1 tw-bg-[#fff]"> Home</span>
-            <span class="tw-cursor-pointer tw-flex tw-text-[#000] tw-items-center tw-justify-center tw-rounded-md tw-p-1 tw-bg-[#fff]">Details</span>
+            <span class="tw-cursor-pointer tw-font-semibold tw-flex tw-text-[#000] tw-border-b-[3px] tw-border-black tw-items-center tw-justify-center tw-px-1 "> Home</span>
+            <span class="tw-cursor-pointer tw-flex tw-text-[#000] tw-border-b-[3px] tw-border-black tw-items-center tw-justify-center  tw-px-1 ">Details</span>
         </div>
-        <div class="tw-w-20 tw-flex tw-text-[#000] tw-items-center tw-justify-center tw-rounded-md tw-p-1 tw-bg-[#fff]"><span class="tw-cursor-pointer">Logout</span></div>
+        <div class="tw-w-20 tw-flex tw-text-[#fff] tw-items-center tw-justify-center tw-rounded-md tw-p-1 tw-bg-black"><span class="tw-cursor-pointer">Logout</span></div>
       </div>
     </div>
 </template>
